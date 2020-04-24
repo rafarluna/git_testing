05cf6657b4c4f18c735eb18209acfa3f824b92cf
Rafael Ramirez Luna <rafinci@hotmail.com>
2020-04-23 23:28:31 -0500

    EL MENSAJE DEL NUEVO CAMBIO

--- src/amed_test.js
+++ src/amed_test.js
@@ -3,3 +3,10 @@
 
 
 // ESTE ES EL SEGUNDO CAMBIO, TODO BIEN
+
+
+
+// este será un cambio incompleto.... para usar amend
+
+// ESTE ES EL CAMBIO FALTANTE, AQUI SE USARÁ 
+// git commit --amend -m "EL MENSAJE DEL NUEVO CAMBIO"
